7d9150c27b3848bc048471d8bff5bdf489b41b33
he-wl <2468305717@qq.com>
2021-07-25 23:28:56 +0800

    Initial commit

---
+++ src/com/hewl/java/HelloWorld.java
@@ -0,0 +1,28 @@
+package com.hewl.java;
+
+/**
+ * @author wen
+ * @date 2021/7/25 - 21:33
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    public void testUpdate(){
+        System.out.println("测试");
+    }
+    /**
+    * 客户id
+    */
+    private int id;
+
+    /**
+    * 客户姓名
+    */
+    private String name;
+
+
+
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: HeWL
+  Date: 2021/7/25
+  Time: 21:49
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>网站</title>
+  </head>
+  <body>
+    <h1 style="color: aqua; text-align: center">Hello World :)</h1>
+  </body>
+</html>
